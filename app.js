cde13e0fde91f33a084e47f3eb11b4f4d5dd4030
Nipun Sathsara Basnayake <nipunsathsara1999@gmail.com>
2024-11-12 09:04:52 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,13 @@
+
+
+
+function addTask(){
+    let txtTitle = document.getElementById("txtTitle").value;
+    let txtTask = document.getElementById("txtTask").value;
+    let divShow = document.getElementById("divShow");
+
+    
+
+
+
+}
