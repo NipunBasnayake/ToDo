b22fbda2de33fca6aa918530fdbaf21d2f719b3e
Nipun Sathsara Basnayake <nipunsathsara1999@gmail.com>
2024-11-12 09:24:13 +0530

    Js Implemented

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
+let divShow = document.getElementById("divShow");
+let taskArray = [];
 
+function sowOnDiv() {
+    let content = '';
+    taskArray.forEach(element => {
+        content += `
+            <div class="card">
+                <div class="card-body">
+                    <h3>${element.txtTitle}</h3>
+                    <p>${element.txtTask}</p>
+                </div>
+            </div>
+        `;
+    });
+    divShow.innerHTML = content;
+}
 
-
-function addTask(){
+function addTask() {
     let txtTitle = document.getElementById("txtTitle").value;
     let txtTask = document.getElementById("txtTask").value;
-    let divShow = document.getElementById("divShow");
 
-    
+    taskArray.push({ txtTitle, txtTask });
+    sowOnDiv();
 
-
-
+    document.getElementById("txtTitle").value = '';
+    document.getElementById("txtTask").value = '';
 }
